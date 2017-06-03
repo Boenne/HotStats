35fbd7c0efc9eb506a1c3ebbd6bd3bb02b51b952
Nicolaj Enøe Simonsen <nicolaj-simonsen@hotmail.com>
2017-06-03 10:53:15 +0200

    Added Wrapper for MessageBox

---
+++ src/HotStats/Wrappers/IMessageBoxWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace HotStats.Wrappers
+{
+    public interface IMessageBoxWrapper
+    {
+        void Show(string message);
+    }
+
+    public class MessageBoxWrapper : IMessageBoxWrapper
+    {
+        public void Show(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
