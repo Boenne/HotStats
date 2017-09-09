79f855069a5fb9bb968d3c34e142fe349854df23
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2017-09-09 20:17:48 +0200

    Added a test project

---
+++ src/HotStats.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HotStats.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HotStats.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("574a6e9e-e79d-4eb7-8c84-660fb904410f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/HotStats.Test/Services/PortraitDownloaderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotStats.Services;
+using Xunit;
+
+namespace HotStats.Test.Services
+{
+    public class PortraitDownloaderTests
+    {
+        private PortraitDownloader _portraitDownloader;
+
+        public PortraitDownloaderTests()
+        {
+            _portraitDownloader = new PortraitDownloader();
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            await _portraitDownloader.DownloadPortraits();
+        }
+    }
+}
