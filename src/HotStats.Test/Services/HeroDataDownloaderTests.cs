4f4d4838a7b10c849015ec8f91ee17402cbf9daa
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2017-09-16 10:21:29 +0200

    Added test for HeroDataDownloader

---
+++ src/HotStats.Test/Services/HeroDataDownloaderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotStats.Services;
+using Xunit;
+
+namespace HotStats.Test.Services
+{
+    public class HeroDataDownloaderTests
+    {
+        [Fact]
+        public async Task Test()
+        {
+            var heroDataDownloader = new HeroDataDownloader();
+            await heroDataDownloader.DownloadData();
+        }
+    }
+}
