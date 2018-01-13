4fffaf4f3422309e1a453d674fd51d68b952357c
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-01-13 18:45:10 +0100

    Added HeroSelected property to DataFilterHasBeenAppliedMessage for charts to not update when a hero is selected

--- src/HotStats/Messaging/Messages/DataFilterHasBeenAppliedMessage.cs
+++ src/HotStats/Messaging/Messages/DataFilterHasBeenAppliedMessage.cs
@@ -2,5 +2,9 @@
 {
     public class DataFilterHasBeenAppliedMessage
     {
+        //TODO
+        //This won't work.
+        // If HeroSelected = true but another filter (e.g. map selected) has been applied the event is still dicarded
+        public bool HeroSelected { get; set; }
     }
 }
