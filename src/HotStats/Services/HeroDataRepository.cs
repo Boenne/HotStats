74efec489ef63625d787fdc653a9a55479114405
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-01-13 16:01:10 +0100

    Added HeroDataRepository

---
+++ src/HotStats/Services/HeroDataRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace HotStats.Services
+{
+    public interface IHeroDataRepository
+    {
+        List<Hero> GetData();
+        void SaveData(List<Hero> heroes);
+    }
+
+    public class HeroDataRepository : IHeroDataRepository
+    {
+        private List<Hero> heroes = new List<Hero>();
+
+        public List<Hero> GetData()
+        {
+            return heroes;
+        }
+
+        public void SaveData(List<Hero> heroes)
+        {
+            this.heroes = heroes;
+        }
+    }
+}
