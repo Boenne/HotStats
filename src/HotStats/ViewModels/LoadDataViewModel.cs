79c4613c351f187c18f438005ff8a7af294f3ad4
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-10-09 15:44:44 +0200

    Optimized the timer of LoadDataViewModel to be more accurate

--- src/HotStats/ViewModels/LoadDataViewModel.cs
+++ src/HotStats/ViewModels/LoadDataViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -17,9 +16,9 @@ namespace HotStats.ViewModels
 {
     public class LoadDataViewModel : ViewModelBase, ILoadDataViewModel
     {
-        private readonly INavigationService navigationService;
         private readonly IDispatcherWrapper dispatcherWrapper;
         private readonly IHeroDataRepository heroDataRepository;
+        private readonly INavigationService navigationService;
         private readonly IParser parser;
         private readonly IReplayRepository replayRepository;
         private bool anyFilesToProcess = true;
@@ -40,49 +39,40 @@ namespace HotStats.ViewModels
             this.heroDataRepository = heroDataRepository;
         }
 
-        public RelayCommand LoadedCommand => new RelayCommand(async () => await Loaded());
-        
+        public RelayCommand LoadedCommand => new RelayCommand(() => LoadData());
+
         public bool AnyFilesToProcess
         {
-            get { return anyFilesToProcess; }
+            get => anyFilesToProcess;
             set { Set(() => AnyFilesToProcess, ref anyFilesToProcess, value); }
         }
 
         public int FilesProcessed
         {
-            get { return filesProcessed; }
+            get => filesProcessed;
             set { Set(() => FilesProcessed, ref filesProcessed, value); }
         }
 
         public int FileCount
         {
-            get { return fileCount; }
+            get => fileCount;
             set { Set(() => FileCount, ref fileCount, value); }
         }
 
         public long ElapsedTime
         {
-            get { return elapsedTime; }
+            get => elapsedTime;
             set { Set(() => ElapsedTime, ref elapsedTime, value); }
         }
 
         public long ApproxTimeLeft
         {
-            get { return approxTimeLeft; }
+            get => approxTimeLeft;
             set { Set(() => ApproxTimeLeft, ref approxTimeLeft, value); }
-        }
-
-        public async Task Loaded()
-        {
-            await LoadData();
         }
 
         public async Task LoadData()
         {
-            FilesProcessed = 0;
-            ElapsedTime = 0;
-            ApproxTimeLeft = 0;
-
             var heroesAccountsFolder = new DirectoryInfo(FilePaths.MyDocuments);
 
             List<Replay> replays;
@@ -91,14 +81,14 @@ namespace HotStats.ViewModels
             {
                 replays = await GetReplaysFromDataFile();
                 var replayFiles = heroesAccountsFolder.GetFiles("*.StormReplay", SearchOption.AllDirectories);
-                
+
                 await dispatcherWrapper.BeginInvoke(() => FileCount = replayFiles.Length);
 
                 var watch = Stopwatch.StartNew();
                 foreach (var replayFile in replayFiles)
                 {
-                    watch.Restart();
-                    if (replays.All(x => x.FileCreationDate != replayFile.CreationTime && x.FileName != replayFile.Name))
+                    if (replays.All(x =>
+                        x.FileCreationDate != replayFile.CreationTime && x.FileName != replayFile.Name))
                     {
                         var replay = await parser.ParseAsync(replayFile.FullName);
                         if (replay != null)
@@ -110,11 +100,14 @@ namespace HotStats.ViewModels
                             replays.Add(replay);
                         }
                     }
-                    await dispatcherWrapper.BeginInvoke(() => FilesProcessed++);
-                    watch.Stop();
-                    await dispatcherWrapper.BeginInvoke(() => ApproxTimeLeft = ElapsedTime / FilesProcessed * (FileCount - FilesProcessed));
-                    await dispatcherWrapper.BeginInvoke(() => ElapsedTime += watch.ElapsedMilliseconds);
+                    await dispatcherWrapper.BeginInvoke(() =>
+                    {
+                        FilesProcessed++;
+                        ElapsedTime = watch.ElapsedMilliseconds;
+                        ApproxTimeLeft = ElapsedTime / FilesProcessed * (FileCount - FilesProcessed);
+                    });
                 }
+                watch.Stop();
             }
             else
             {
