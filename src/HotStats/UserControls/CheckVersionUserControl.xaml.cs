3e3e53eda824ad91d162a452eef5cb873ec02492
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-10-16 13:16:37 +0200

    New UserControl to check for newer version of program

---
+++ src/HotStats/UserControls/CheckVersionUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HotStats.UserControls
+{
+    /// <summary>
+    ///     Interaction logic for SetPlayerNameUserControl.xaml
+    /// </summary>
+    public partial class CheckVersionUserControl : UserControl
+    {
+        public CheckVersionUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
