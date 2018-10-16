11afb05d50dfd9190526276120c8a73f869bff99
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-10-16 13:20:10 +0200

    ViewModel for CheckVersion

---
+++ src/HotStats/ViewModels/CheckVersionViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using HotStats.Messaging;
+using HotStats.Navigation;
+using HotStats.Services;
+
+namespace HotStats.ViewModels
+{
+    public class CheckVersionViewModel : ViewModelBase, ICheckVersionViewModel
+    {
+        private readonly INavigationService navigationService;
+        private readonly IVersionChecker versionChecker;
+        private bool isOutDated;
+
+        public CheckVersionViewModel(IMessenger messenger, INavigationService navigationService,
+            IVersionChecker versionChecker) : base(messenger)
+        {
+            this.navigationService = navigationService;
+            this.versionChecker = versionChecker;
+        }
+
+        public bool IsOutDated
+        {
+            get => isOutDated;
+            set { Set(() => IsOutDated, ref isOutDated, value); }
+        }
+
+        public RelayCommand ContinueCommand => new RelayCommand(Continue);
+        public RelayCommand DownloadNewVersionCommand => new RelayCommand(DownloadNewVersion);
+        public RelayCommand LoadedCommand => new RelayCommand(() => IsVersionOutDated());
+
+        public async Task IsVersionOutDated()
+        {
+            var outDated = await versionChecker.IsVersionOutdated();
+            if (isOutDated)
+                IsOutDated = outDated;
+            else
+                navigationService.NavigateTo(NavigationFrames.DownloadPortraits);
+        }
+
+        public void DownloadNewVersion()
+        {
+            Process.Start("https://github.com/Boenne/HotStats/raw/master/Program/Program.zip");
+        }
+
+        public void Continue()
+        {
+            navigationService.NavigateTo(NavigationFrames.DownloadPortraits);
+        }
+    }
+
+    public interface ICheckVersionViewModel
+    {
+        bool IsOutDated { get; set; }
+        RelayCommand ContinueCommand { get; }
+        RelayCommand DownloadNewVersionCommand { get; }
+        RelayCommand LoadedCommand { get; }
+    }
+}
