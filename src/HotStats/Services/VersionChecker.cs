db13a45a14d8e91cae4913482042b1a5ba3bd47b
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-10-16 13:18:03 +0200

    Service to check for a newer version of the program

---
+++ src/HotStats/Services/VersionChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HotStats.Services
+{
+    public class VersionChecker : IVersionChecker
+    {
+        public async Task<bool> IsVersionOutdated()
+        {
+            try
+            {
+                var stream =
+                    await WebClients.WebClient.OpenReadTaskAsync(
+                        "https://github.com/Boenne/HotStats/raw/master/Program/version.txt");
+                using (stream)
+                using (var streamReader = new StreamReader(stream))
+                {
+                    var version = await streamReader.ReadLineAsync();
+                    return ConfigurationManager.AppSettings["version"] != version;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+    public interface IVersionChecker
+    {
+        Task<bool> IsVersionOutdated();
+    }
+}
