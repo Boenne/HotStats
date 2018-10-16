d1e53f6da1a4d99276afd7dd5be813b80f3385cc
DESKTOP-7JBUI3Q\nicol <nicolaj-simonsen@hotmail.com>
2018-10-16 13:19:13 +0200

    Service to check whether or not the user is online

---
+++ src/HotStats/Services/InternetConnectivityService.cs
@@ -0,0 +1,25 @@
+﻿namespace HotStats.Services
+{
+    public class InternetConnectivityService : IInternetConnectivityService
+    {
+        public bool IsOnline()
+        {
+            try
+            {
+                using (WebClients.WebClient.OpenRead("http://clients3.google.com/generate_204"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+    public interface IInternetConnectivityService
+    {
+        bool IsOnline();
+    }
+}
